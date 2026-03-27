807f6d03c9a49858652f5504c059371b9acf38e3
BVasquez07 <bvasq07@gmail.com>
2026-03-27 02:00:57 -0400

    pushing updates

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { AuthService } from "../../core/services/authService";
 
 @Component({
